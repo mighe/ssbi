3d6ae1bf3752d8a90657b7368b7aaf4f2e70e6a4
Michele Della Torre <michele.dellatorre@gmail.com>
2014-03-10 08:37:57 +0100

    Main supports folder as input

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,64 @@
 import it.mighe.ssbi.ProgramExecutor
+import java.io.File
 
 object Main extends App {
 
-  def benchmark(f: => Unit): Long = {
+  if (args.isEmpty) { printUsage() }
+
+  val programsExecutionTime = new scala.collection.mutable.LinkedHashMap[String, Long]
+
+  for(programPath <- inputFiles) {
+
+    Console.println("executing " + programPath.getName + "...")
+
+    val program = scala.io.Source.fromFile(programPath).mkString
+    val executor = new ProgramExecutor(System.out, System.in)
+    val elapsedTime = benchmark { executor.execute(program) }
+
+    programsExecutionTime(programPath.getName) = elapsedTime
+  }
+
+  printResults(programsExecutionTime)
+
+  // -------
+
+  private def benchmark(f: => Unit): Long = {
     val startTime = System.currentTimeMillis()
     f
     System.currentTimeMillis() - startTime
   }
 
-  if (args.isEmpty) {
-    println("usage: scala Main <inputFileName>")
+  private def printUsage() {
+    println("usage: scala Main <input file pattern>")
+    println("examples:")
+    println("  scala Main my_bf_program.b --> executes only my_bf_program.b")
+    println("  scala Main my_folder --> executes all *.b programs in my_folder")
     System.exit(1)
   }
 
-  val fileName = args(0)
-  val program = scala.io.Source.fromFile(fileName).mkString
+  private def inputFiles = {
+    val file = new File(args(0))
 
-  val executor = new ProgramExecutor(System.out, System.in)
+    if (file.isFile) {
+      Array(file)
+    }
+    else {
+      file.listFiles().filter(_.getName.endsWith(".b"))
+    }
+  }
 
-  val elapsedTime = benchmark { executor.execute(program) }
-  
-  Console.println()
-  Console.println("Program correctly executed in " + elapsedTime + " ms")
+  private def printResults(results: scala.collection.mutable.Map[String, Long]) {
+    Console.println()
+    Console.println("Execution times:")
+    results.foreach {
+      case (program, time) => Console.println(s">>>> $program executed in $time ms")
+    }
+
+    val totalExecutionTime = results.valuesIterator.foldLeft(0L)(_ + _)
+    Console.println()
+    Console.println("-------------------------------------------")
+    Console.println(s"Total execution time: $totalExecutionTime ms")
+  }
+
 
 }
