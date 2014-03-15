81aabc90281d6235cf5dbb92bd1c04a1386b26ff
Michele Della Torre <michele.dellatorre@gmail.com>
2014-03-15 20:05:01 +0100

    added optimization for consecutive value and pointer adjustments
    Optimizer code is ugly

--- src/main/scala/it/mighe/ssbi/Optimizer.scala
+++ src/main/scala/it/mighe/ssbi/Optimizer.scala
@@ -1,7 +1,58 @@
 package it.mighe.ssbi
 
+import scala.collection.mutable.ArrayBuffer
+import it.mighe.ssbi.instructions.{AdjustPointerInstruction, AdjustValueInstruction}
+
 class Optimizer {
-  def optimize(program: Array[Instruction]) = {
-    program
+  def optimize(program: Array[Instruction]): Array[Instruction] = {
+    if(program.isEmpty) return program
+
+    val optimized = new ArrayBuffer[Instruction]()
+
+    var index = 0
+
+    while(index < program.length) {
+      program(index) match {
+        case x: AdjustValueInstruction =>
+          val result = compactValueSequence(program, index)
+          optimized += result._1
+          index += result._2
+        case x: AdjustPointerInstruction =>
+          val result = compactShiftSequence(program, index)
+          optimized += result._1
+          index += result._2
+        case x =>
+          optimized += x
+          index += 1
+      }
+    }
+
+
+    optimized.toArray
   }
+
+  def compactValueSequence(program: Array[Instruction], index: Int) = {
+    var sum = 0
+    var length = 0
+
+    while( (index + length < program.length) && program(index + length).isInstanceOf[AdjustValueInstruction] ) {
+      sum += program(index + length).asInstanceOf[AdjustValueInstruction].offset
+      length += 1
+    }
+
+    (new AdjustValueInstruction(sum), length)
+  }
+
+  def compactShiftSequence(program: Array[Instruction], index: Int) = {
+    var sum = 0
+    var length = 0
+
+    while( (index + length < program.length) && program(index + length).isInstanceOf[AdjustPointerInstruction] ) {
+      sum += program(index + length).asInstanceOf[AdjustPointerInstruction].offset
+      length += 1
+    }
+
+    (new AdjustPointerInstruction(sum), length)
+  }
+
 }
---
+++ src/test/scala/it/mighe/ssbi/OptimizerSpec.scala
@@ -0,0 +1,66 @@
+package it.mighe.ssbi
+
+import org.scalatest.{Matchers, FlatSpec}
+import it.mighe.ssbi.instructions._
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
+import java.io.ByteArrayInputStream
+import CustomMatchers._
+
+class OptimizerSpec extends FlatSpec with Matchers {
+
+  def fixture = new {
+    val parser = new Parser(new ByteOutputStream(), new ByteArrayInputStream(Array()))
+    val optimizer = new Optimizer
+  }
+
+  it can "optimize an empty program" in {
+    val instructions = fixture.optimizer.optimize(Array())
+    instructions should equal(Array[Instruction]())
+  }
+
+  it should "not modify input array" in {
+    val source = "+++ +++ +++ , [-] >> . "
+    val instructions = fixture.parser.parse(source)
+
+    fixture.optimizer.optimize(instructions)
+
+    instructions should have length 16
+  }
+
+  it should "optimize consecutive increments" in {
+    val instructions = fixture.parser.parse("+++ +++ +++")
+    val optimized = fixture.optimizer.optimize(instructions)
+
+    optimized should have length 1
+    optimized(0) should beAValueInstructionWithOffset(9)
+  }
+
+  it should "optimize consecutive decrements" in {
+    val instructions = fixture.parser.parse("- . ---")
+    val optimized = fixture.optimizer.optimize(instructions)
+
+    optimized should have length 3
+    optimized(0) should beAValueInstructionWithOffset(-1)
+    optimized(1) shouldBe a [WriteInstruction]
+    optimized(2) should beAValueInstructionWithOffset(-3)
+  }
+
+  it should "optimize consecutive right shifts" in {
+    val instructions = fixture.parser.parse(">>> >>>")
+    val optimized = fixture.optimizer.optimize(instructions)
+
+    optimized should have length 1
+    optimized(0) should beAPointerInstructionWithOffset(6)
+  }
+
+  it should "optimize consecutive left shifts" in {
+    val instructions = fixture.parser.parse(">>> , <<")
+    val optimized = fixture.optimizer.optimize(instructions)
+
+    optimized should have length 3
+    optimized(0) should beAPointerInstructionWithOffset(3)
+    optimized(1) shouldBe a [ReadInstruction]
+    optimized(2) should beAPointerInstructionWithOffset(-2)
+  }
+
+}
